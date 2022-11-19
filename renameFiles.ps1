fcd82dfdb83c3d08c4170430e26e9b1556dd63c8
vovka <9141778236@mail.ru>
2022-11-19 23:40:15 +1000

    v1.0.1

--- renameFiles.ps1
+++ renameFiles.ps1
@@ -7,10 +7,10 @@ Param (
     [string]$Dest="",
     [string]$LeadingChar="",
     [switch]$Recurse,
-    [switch]$Debug,
+    [switch]$Copy,
+    [switch]$isDebug,
     [int]$MaxNumber=99999999
 )
-$isDebug = $Debug.IsPresent;
 
 if ($Source -eq "") { $Source = ".\"; }
 if ($FileMask -eq "") { $FileMask = "*.mp3"; }
@@ -19,7 +19,7 @@ if ($LeadingChar -eq "") {
     $LeadingChar = (Get-ChildItem ([System.IO.Path]::GetTempFileName())).BaseName + "-";
 }
 
-if ($isDebug) {
+if ($isDebug.IsPresent) {
     echo $LeadingChar
     echo $Source
     echo $srcFiles
@@ -32,8 +32,21 @@ $p = @{
     'Filter'=$FileMask
     'Recurse'=$Recurse.IsPresent
 }
+#$files = Get-ChildItem @p
 [System.Collections.ArrayList]$files = Get-ChildItem @p
+if ($isDebug.IsPresent) {
+    echo "files ================================="
+    echo $files|select fullname;
+    echo "files.count: $($files.count)"
+    echo "files.getType: $($files.getType())"
+    $files_.ForEach({
+        echo $_
+    });
+}
 
+#exit 0
+
+# подготовить массив с именами в которые скопировать (переименовать)
 $newFiles = [ordered]@{};
 $rv = Get-Random -Minimum 1 -Maximum $MaxNumber;
 while ($files.Count -gt 0) {
@@ -43,8 +56,16 @@ while ($files.Count -gt 0) {
     $newFiles.Add($rv, $files[0]);
     $files.removeAt(0);
 }
+if ($isDebug.IsPresent) {
+    echo "newFiles ================================="
+    $newFiles;
+}
 
 $newFiles.Keys.ForEach({
+    if ($isDebug.IsPresent) {
+        echo $_;
+
+    }
     $f = $newFiles.$_;
     $f.moveTo("$($f.DirectoryName)\$($LeadingChar)$(([string]$_).PadLeft(8,'0'))$($f.Extension)")
 });
